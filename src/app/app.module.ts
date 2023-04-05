48a94a2da3cade35604b302d71ca1b5006fde79f
Schwabegger <20172031@atn.ac.at>
2023-04-05 03:03:58 +0200

    todos with pockedbase; todos page; delete todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { TodoListComponent } from './todo-list/todo-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule // Add HttpClientModule to imports
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import PocketBase from 'pocketbase';
 
-interface Task {
-  name: string;
-  completed: boolean;
+export interface ITodo {
+  id: string;
+  title: string;
+  tasks: {
+    id: string;
+    name: string;
+    completed: boolean;
+  }[];
 }
 
-interface Todo {
+export class Todo {
+  constructor(id: string, title: string, tasks: { id: string, name: string, completed: boolean }[]) {
+    this.id = id;
+    this.title = title;
+    this.tasks = tasks;
+  }
+  id: string;
   title: string;
-  tasks: Task[];
+  tasks: {
+    id: string;
+    name: string;
+    completed: boolean;
+  }[];
 }
 
 @Component({
@@ -15,31 +33,46 @@ interface Todo {
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
+
 export class TodoListComponent implements OnInit {
+  todos: any[] = [];
+  pb = new PocketBase('https://to-dos.pockethost.io');
 
-  todos: Todo[] = [
-    {
-      title: 'Grocery Shopping',
-      tasks: [
-        { name: 'Buy milk', completed: false },
-        { name: 'Buy bread', completed: true },
-        { name: 'Buy eggs', completed: false }
-      ]
-    },
-    {
-      title: 'House Cleaning',
-      tasks: [
-        { name: 'Clean bathroom', completed: true },
-        { name: 'Vacuum living room', completed: true },
-        { name: 'Wash dishes', completed: false }
-      ]
-    },
-    // Add more todos here
-  ];
+  constructor(private http: HttpClient, private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  deleteTodo(id: string) {
+    this.deleteTodoAsync(id).then(() => {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    });
   }
 
+  async deleteTodoAsync(id: string): Promise<void> {
+    await this.pb.collection('todos').delete(id);
+  }
+
+  async ngOnInit() {
+    // this.pb.collection('todos').subscribe('*', function (e) {
+    //   console.log(e.record);
+    // });
+
+    const records = await this.pb.collection('todos').getList(1, 50, {
+      sort: '-created',
+      expand: 'tasks',
+    });
+    this.todos = structuredClone(records.items);
+
+    // this.http.get<Todo[]>('https://to-dos.pockethost.io/api/collections/todos/records').subscribe(
+    //   response => {
+    //     this.todos = response;
+    //   },
+    //   error => {
+    //     console.log('Error retrieving todos:', error);
+    //   }
+    // );
+  }
+
+  edit(id: string) {
+    console.log('edit', id);
+    this.router.navigate(['/edit', id]);
+  }
 }
